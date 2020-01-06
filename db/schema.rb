54ea6bb952874dddf4ddb5ffa8e66ea32b24eb98
hrk <hiro5210w@gmail.com>
2020-01-06 05:03:52 +0000

    カラム追加課題

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_061831) do
+ActiveRecord::Schema.define(version: 2020_01_06_042746) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
